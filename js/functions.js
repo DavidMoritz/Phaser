2d97ffd3294a04926555c226d7f759233a5b86a4
Moritz, David <Moritz.David@principal.com>
2015-02-09 11:00:02 -0600

    Add online files

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+var showCode = function() {
+    document.getElementById("code").className = "show";
+	document.getElementsByTagName("body")[0].style.cssText = "";
+};
+var hideCode = function() {
+    document.getElementById("code").className = "hide";
+};
